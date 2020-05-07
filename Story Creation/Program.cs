8cdecb8505136b59efd4acb934491bd8d52153e4
Veer Shah <veer.shah.uk@gmail.com>
2020-05-07 21:31:52 +0100

    inital import

---
+++ Story Creation/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Threading.Tasks.Dataflow;
+
+namespace Story_Creation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("How old are you?");
+            string age = Console.ReadLine();
+            Console.WriteLine("What is your superpower?");
+            string power = Console.ReadLine();
+            Console.WriteLine("What is your unbeatable weapon?");
+            string weapon = Console.ReadLine();
+            Console.WriteLine("Who is your nemesis?");
+            string enemy = Console.ReadLine();
+       
+
+
+            Console.WriteLine("Once upon a time there was someone called "  + name +   " he was "  + age +  " years old and lived with his mum and dad. One day " + enemy + " flew over to the city he was living in and decided to burn it to ashes. "  + name +  " came along and with his "  + power +  " and " + weapon + " slayed the mighty " + enemy + " and everyone lived happily every after.");
+        }
+    }
+}
